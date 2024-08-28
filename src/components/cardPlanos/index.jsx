96c6e290574ea49ad21e5d318baa33f1fb2d882d
Lorenzo <81311825+Acquesta@users.noreply.github.com>
2024-08-27 21:06:46 -0300

    Add files via upload

--- src/components/cardPlanos/index.jsx
+++ src/components/cardPlanos/index.jsx
@@ -1,16 +1,22 @@
 import './style.css'
 
-function CardPlano() {
+function CardPlano({ fundo, corBotao, borda, titulo, inputs, textoBotao, img }) {
     return ( 
         <div className='cardPlano'>
-            <h3>Individual</h3>
+            <h3 style={{backgroundColor: fundo}}>{titulo}</h3>
             <div className="cardPlanos__infos">
-                <p>1 Usuário</p>
-                <h4>10 vídeos <span className='bold'>R$15</span></h4>
+                {inputs.map(plano => (
+                    <div key={plano.title} className="infos__texto">
+                        <p>{plano.label}</p>
+                        <h4>{plano.input}<span className='bold'> {plano.span}</span></h4>
+                    </div>
+                ))}
+                <img src={img} alt="" />
+                <button style={{backgroundColor: fundo, color: corBotao, border: borda == false ? 'none' : '2px solid #EBE9E9' }} className="cardPlano__button">
+                    {textoBotao}
+                </button>
+                
             </div>
-            <button className="cardPlano__button">
-                Cadastrar
-            </button>
         </div>
      );
 }
--- src/components/planos/index.jsx
+++ src/components/planos/index.jsx
@@ -1,14 +1,65 @@
 import CardPlano from '../cardPlanos';
 import './style.css'
+import planoCorporativo from '../../assets/planoCorporativo.png'
+
+const planos =[
+    {
+        title: 'Individual',
+        inputs: [
+            {
+                label: '1 Usuário',
+                input: '10 Vídeos', 
+                span: 'R$15'
+            }
+        ],
+        fundo: '#6975E8',
+        corBotao: '#EBE9E9',
+        borda: false,
+        textoBotao: 'Cadastrar'
+    },
+    {
+        title: 'Profissional',
+        inputs: [
+            {
+                label: '1 - 10 Usuários',
+                input: 'Vídeos Ilimitados', 
+                span: 'R$40'
+            },
+            {
+                label: '+10 Usuários',
+                input: 'Vídeos Ilimitados', 
+                span: 'R$20'
+            }
+        ],
+        textoBotao: 'Cadastrar'
+    },
+    {
+        title: 'Corporativo',
+        inputs: [],
+        img: planoCorporativo,
+        textoBotao: 'Entrar em contato'
+    },
+] 
 
 function Planos() {
     return ( 
         <div className='container__planos'>
             <h2 className='titulo__planos'>Planos</h2>
             <div className="planos">
-                <CardPlano />
-                <CardPlano />
-                <CardPlano />
+                {planos.map(plano => 
+                    <CardPlano
+                        key = {plano.title}
+                        fundo = {plano.fundo}
+                        corBotao = {plano.corBotao}
+                        borda = {plano.borda}
+                        titulo = {plano.title}
+                        inputs = {plano.inputs}
+                        textoBotao = {plano.textoBotao}
+                        img = {plano.img}
+                    />
+                )}
+                
+                
             </div>
         </div>
      );
